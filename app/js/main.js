93073dd599f41bb06701d03ef9f6ce09877e3af2
Timohinser <timohinser33@gmail.com>
2020-02-17 13:17:50 +0300

    fixed slider in section weekly-products

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
 $(function(){
    $('.weekly-products__slider').slick({
-      
+      prevArrow:'<button type="button" class="slick-prev"><img src="images/icons/prewArrow.png" alt=""></button>',
+      nextArrow:'<button type="button" class="slick-next"><img src="images/icons/nextArrow.png" alt=""></button>',      
    });
    // var mixer = mixitup('.someClass');   
 });
